bca0ad93fa1e0b4e504b549f68007d01f57ebf69
LiYi <li.yi@cloud-ace.jp>
2020-04-30 16:23:00 +0900

    サンプル説明： https://mebee.info/2019/12/06/post-4494/#outline__2

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,91 @@
 <template>
-  <h1>Hello world!</h1>
+  <section class="container">
+    <div>      
+      <v-app id="inspire">
+        <v-data-table
+          :headers="headers"
+          :items="lists"
+          :items-per-page="10"
+          class="elevation-1"
+        >
+          <template v-slot:item.title="{ item }">
+            <p class="font-weight-medium">{{ item.title }}</p>
+          </template>
+          <template v-slot:item.id="{ item }">
+            <nuxt-link :to="`/users/${item.id}`">
+              <v-chip :color="getColor(item.id)" dark>{{ item.id }}</v-chip>
+            </nuxt-link>
+          </template>
+          <template v-slot:item.completed="{ item }">
+           {{ getFlg(item.completed) }}
+          </template>
+        </v-data-table>
+      </v-app>
+    </div>
+  </section>
 </template>
+ 
+<script>
+export default {
+  data () {
+    return {
+      headers: [
+        {
+          text: 'タイトル',
+          align: 'left',
+          sortable: false,
+          value: 'title',
+        },
+        { text: 'id', value: 'id' },
+        { text: '完了フラグ', value: 'completed' },
+      ],
+    }
+  },
+  methods: {
+    getColor (calories) {
+      if (calories % 2 == 0) return 'red'
+      return 'green'
+    },
+    getFlg (flg) {      
+      if (flg) return '完了'
+      else return '未完了'
+    },
+  },
+  async asyncData({ app }) {
+    const baseUrl = 'https://jsonplaceholder.typicode.com/todos/';
+    const response = await app.$axios.$get(baseUrl);
+    return { lists: response };
+  }
+}
+</script>
+ 
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+ 
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+ 
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+ 
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ pages/users/_id.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <v-app id="inspire">
+            <v-card
+            class="mx-auto"
+            max-width="450"
+            >
+                <v-card-text>
+                <p class="display-1 text--primary">UserID {{userId}}</p>
+                <div class="text--primary">
+                    <v-icon>mdi-account</v-icon>{{user.name}}<br>
+                    <v-icon>mdi-alpha</v-icon>{{user.email}}
+                </div>
+                </v-card-text>
+            </v-card>
+        </v-app>
+    </div>
+</template>
+ 
+<script>
+export default {
+    data(){
+        return{
+            userId: this.$route.params.id
+        }
+    },
+    async asyncData ({route,app}) {
+        const user = await app.$axios.$get(`https://jsonplaceholder.typicode.com/users/${route.params.id}`)
+        return{
+            user
+        }
+    }
+}
+</script>
